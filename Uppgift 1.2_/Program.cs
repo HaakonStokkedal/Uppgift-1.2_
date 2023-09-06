c61ad90fcb1eaa6e899a709cf3491079780fe543
hast06001 <hast06001@E-PW024NW1>
2023-09-06 13:05:00 +0200

    Add project files.

---
+++ Uppgift 1.2_/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_1._2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Filen kunde inte hittas, funkar yoghurt istället?");
+            Console.Clear();
+        }
+    }
+}
